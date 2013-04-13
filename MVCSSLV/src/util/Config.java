ea1b19e8d438cfb520f40b4d772d99028903297d
Nikolay <nikolay.borovoy@gmail.com>
2013-04-13 14:26:54 +0300

    merge done

--- MVCSSLV/src/util/Config.java
+++ MVCSSLV/src/util/Config.java
@@ -3,22 +3,6 @@ package util;
 public interface Config {
 	public static final String APP_VERSION = "v3.0.5";
 	public static final String CONTEXT_ROOT = "/java2/";
-<<<<<<< HEAD
-	public static final String NOT_LOGGED_IN = "Not Logged In";
-	public static final String LOGGED_IN = "Logged In";
-	public static final String NO_SUCH_USER = "No Such User";
-	public static final String LOGGED_OUT = "Logged Out";
-	public static final String EMPTY = "";
-	public static final String AD_DELETED = "Advertisment has been removed.";
-	public static final int ADS_PER_MAIN_PAGE = 20;
-	public static final int ADS_PER_LOGIN_PAGE = 10;
-	public static final int PAGES_IN_LINE = 20;
-	public static final String USERNAME = "username";
-	public static final String ACTION_LOGIN = "login";
-	public static final String ACTION_LOGOUT = "logout";
-	public static final String ACTION_DELETE = "delete";
-	public static final String ACTION_NOT_AUTHORIZED = "Not Authorized!";
-=======
     public static final String NOT_LOGGED_IN = "Not Logged In";
     public static final String LOGGED_IN = "Logged In";
     public static final String NO_SUCH_USER = "No Such User";
@@ -36,5 +20,4 @@ public interface Config {
     public static final String ACTION_VIEW = "view";
     public static final String ACTION_EDIT = "edit";
     public static final String ACTION_NOT_AUTHORIZED = "Not Authorized!";
->>>>>>> origin/kolya12042013
 }
