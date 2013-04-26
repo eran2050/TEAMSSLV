491419ea2c233b82f691bab10701445d5a5f7cf6
Alex Volkoff <suxlv2@gmail.com>
2013-04-26 22:53:45 +0300

    merge conflict

--- MVCSSLV/src/domain/users/Users.java
+++ MVCSSLV/src/domain/users/Users.java
@@ -8,50 +8,50 @@ import javax.persistence.Table;
 @Table(name = "users")
 public class Users {
 
-    @Id
-    private String id;
-    private String name;
-    private String surName;
-    private String eMail;
-    private String phone;
+	@Id
+	private String id;
+	private String name;
+	private String surName;
+	private String eMail;
+	private String phone;
 
-    public String getId() {
-        return id;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public void setId(String id) {
-        this.id = id;
-    }
+	public void setId(String id) {
+		this.id = id;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getSurName() {
-        return surName;
-    }
+	public String getSurName() {
+		return surName;
+	}
 
-    public void setSurName(String surName) {
-        this.surName = surName;
-    }
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
 
-    public String geteMail() {
-        return eMail;
-    }
+	public String geteMail() {
+		return eMail;
+	}
 
-    public void seteMail(String eMail) {
-        this.eMail = eMail;
-    }
+	public void seteMail(String eMail) {
+		this.eMail = eMail;
+	}
 
-    public String getPhone() {
-        return phone;
-    }
+	public String getPhone() {
+		return phone;
+	}
 
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
 }
